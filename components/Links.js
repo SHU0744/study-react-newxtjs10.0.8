48e0489c5e0b877baf89e894e97db488b2cb95bc
SHU0744 <miyavi.senshu@gmail.com>
2023-08-24 09:44:49 +0900

    css modules

--- components/Links.js
+++ components/Links.js
@@ -1,14 +1,14 @@
-import styles from "../styles/Home.module.css";
+import styles from "./Links.module.css";
 export function Links() {
   return (
     <div className={styles.grid}>
       <a href="https://nextjs.org/docs" className={styles.card}>
-        <h3>Documentation &rarr;</h3>
+        <h3 className={styles.title}>Documentation &rarr;</h3>
         <p>Find in-depth information about Next.js features and API.</p>
       </a>
 
       <a href="https://nextjs.org/learn" className={styles.card}>
-        <h3>Learn &rarr;</h3>
+        <h3 className={styles.title}>Learn &rarr;</h3>
         <p>Learn about Next.js in an interactive course with quizzes!</p>
       </a>
 
@@ -16,7 +16,7 @@ export function Links() {
         href="https://github.com/vercel/next.js/tree/master/examples"
         className={styles.card}
       >
-        <h3>Examples &rarr;</h3>
+        <h3 className={styles.title}>Examples &rarr;</h3>
         <p>Discover and deploy boilerplate example Next.js projects.</p>
       </a>
 
@@ -24,7 +24,7 @@ export function Links() {
         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         className={styles.card}
       >
-        <h3>Deploy &rarr;</h3>
+        <h3 className={styles.title}>Deploy &rarr;</h3>
         <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
       </a>
     </div>
