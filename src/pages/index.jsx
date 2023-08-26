e37cae4cd51bee57e1b7fb1372ae677ff5b7aaac
SHU0744 <miyavi.senshu@gmail.com>
2023-08-27 06:39:05 +0900

    イベント処理（クリック）

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,21 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 123;
+  const handleClick = useCallback((e) => {
+    console.log(e.target);
+    alert(foo);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>TOPページ</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>クリック</button>
       <Main page={"index"} />
       <Footer />
     </div>
