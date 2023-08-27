eecade43692774d38545ea54edffbb7461c37759
SHU0744 <miyavi.senshu@gmail.com>
2023-08-27 17:16:18 +0900

    useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,21 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const foo = 123;
   const handleClick = useCallback((e) => {
     console.log(e.target);
     alert(foo);
+  }, []);
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "red";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
   return (
     <div className={styles.container}>
