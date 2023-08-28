cc7eba2b9170cc8950a5a79c43e847b4950895f7
SHU0744 <miyavi.senshu@gmail.com>
2023-08-28 21:22:15 +0900

    useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,19 +3,18 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 123;
-  const handleClick = useCallback((e) => {
-    console.log(e.target);
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
   useEffect(() => {
-    console.log("マウント時");
+    // console.log("マウント時");
     document.body.style.backgroundColor = "red";
     return () => {
-      console.log("アンマウント時");
+      // console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -25,6 +24,7 @@ export default function Home() {
         <title>TOPページ</title>
       </Head>
       <Header />
+      <h1>{count}</h1>
       <button onClick={handleClick}>クリック</button>
       <Main page={"index"} />
       <Footer />
