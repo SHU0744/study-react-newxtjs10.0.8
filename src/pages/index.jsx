c49595f7d5d54a63267f05ef444adb7fcc0a88ae
SHU0744 <miyavi.senshu@gmail.com>
2023-08-30 06:30:53 +0900

    useCallback useEffectについて

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,25 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount((prev) => prev + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((prev) => prev + 1);
+    }
+  }, [count]);
   useEffect(() => {
     // console.log("マウント時");
+    // console.log(count);
     document.body.style.backgroundColor = "red";
     return () => {
       // console.log("アンマウント時");
+      // console.log(count);
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
   return (
     <div className={styles.container}>
       <Head>
