0af121ad42f5ede2c36ac5440e04d10ae9e5b950
SHU0744 <miyavi.senshu@gmail.com>
2023-09-02 18:56:32 +0900

    コンポーネント間 sateのリストアップ

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,10 +1,11 @@
 import styles from "src/components/Headline/Headline.module.css";
 export function Headline(props) {
-  const { page, children } = props;
+  const { page, handlePop, children } = props;
   return (
     <div>
       <h1 className={styles.title}>{page} page</h1>
-      <p className={styles.description}>Get started by editing {children}</p>
+      <p className={styles.description}>リンクの数は {children}</p>
+      <button onClick={handlePop}>リンク減らす</button>
     </div>
   );
 }
--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,30 +1,10 @@
 import styles from "src/components/Links/Links.module.css";
-const ITEMS = [
-  {
-    href: "https://nextjs.org/docs",
-    title: "Documentation →",
-    desc: "Find in-depth information about Next.js features and API.",
-  },
-  {
-    href: "https://nextjs.org/learn",
-    title: "Learn →",
-    desc: "Learn about Next.js in an interactive course with quizzes!",
-  },
-  {
-    href: "https://github.com/vercel/next.js/tree/master/examples",
-    title: "Examples →",
-    desc: "Discover and deploy boilerplate example Next.js projects.",
-  },
-  {
-    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Deploy →",
-    desc: "Instantly deploy your Next.js site to a public URL with Vercel.",
-  },
-];
-export function Links() {
+
+export function Links(props) {
+  const { items } = props;
   return (
     <div className={styles.grid}>
-      {ITEMS.map((item) => {
+      {items.map((item) => {
         return (
           <a key={item.href} href={item.href} className={styles.card}>
             <h3 className={styles.title}>{item.title}</h3>
