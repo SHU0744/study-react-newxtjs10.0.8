ceaff91333bbde702f7e08d43d3488ab95d1eac1
SHU0744 <miyavi.senshu@gmail.com>
2023-09-07 06:27:15 +0900

    useSWRで非同期処理

---
+++ src/hooks/usePosts.jsx
@@ -0,0 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error("エラーが発生したよ");
+  }
+
+  const json = res.json();
+  return json;
+};
+
+export const usePosts = () => {
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  return { data, error, isLoading, isEmpty: data && data.length === 0 };
+};
