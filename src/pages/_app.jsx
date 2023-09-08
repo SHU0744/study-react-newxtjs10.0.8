9140e7a8a101ab71e12e32648926e0e5f57e8fcc
SHU0744 <miyavi.senshu@gmail.com>
2023-09-09 07:31:18 +0900

    不要なファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,19 +1,13 @@
 import "src/styles/globals.css";
 import Head from "next/head";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputAdd } from "src/hooks/useInputAdd";
-import { useBgColor } from "src/hooks/useBgColor";
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const InputAdd = useInputAdd();
-  useBgColor();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...InputAdd} />
+      <Component {...pageProps} />
     </>
   );
 };
